5b3fb9da8d5d1b441c64e74530b14a2f863f78b4
Sbytes <Santoshaji63@gmail.com>
2026-05-31 22:43:47 +0530

    Rename 'stop' method to 'kill' in Processor class

--- src/core/abstracts/processor.py
+++ src/core/abstracts/processor.py
@@ -4,17 +4,13 @@ from abc import ABC, abstractmethod
 
 # @abstract class
 class Processor(ABC):
-    @abstractmethod
-    def pre_process(self, data):
-        """Pre-process raw data before main processing."""
-        pass
     
     @abstractmethod
     def start(self):
         pass
     
     @abstractmethod
-    def stop(self):
+    def kill(self):
         pass
     
     @abstractmethod
