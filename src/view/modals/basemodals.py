dc1be55ddb222b179815f173c44d41507592a06a
Sbytes <Santoshaji63@gmail.com>
2026-05-31 22:52:50 +0530

    Add new request models for server management

--- src/view/modals/basemodals.py
+++ src/view/modals/basemodals.py
@@ -1,5 +1,5 @@
 from pydantic import BaseModel
-from typing import Optional
+from typing import Any, List, Optional
 
 
 class AddServerRequest(BaseModel):
@@ -8,5 +8,35 @@ class AddServerRequest(BaseModel):
     server_name: str
     root_path: str
     runnable_command: str
-    port_no: Optional[int] = None
-    notes: Optional[str] = None 
+    notes: Optional[str] = None # not using as of now
+    clone_from : Optional[str] = None # used for cloning server details from an existing server (except server_id and server_name)
+    
+class BatchStartRequest(BaseModel):
+    server_ids: List[str] 
+    delay_seconds: Optional[int] = 2  
+
+class CreateSessionRequest(BaseModel):
+    session_name: str
+
+class UpdateSessionRequest(BaseModel):
+    session_name: str
+    
+class SessionInfoHolder(BaseModel):
+    session_id: str
+    server_lists: List[Any]
+    
+class ServerInterActionRequest(BaseModel):
+    action: str
+    source_session_info: SessionInfoHolder
+    target_session_info: SessionInfoHolder
+
+class ProxySettingsRequest(BaseModel):
+    root_path: Optional[str] = None
+
+class BasicSettingsRequest(BaseModel):
+    refresh_interval: Optional[int] = None
+    terminal: Optional[str] = None
+
+class UpdateAppSettingsRequest(BaseModel):
+    basic_settings: BasicSettingsRequest
+    advanced_settings: dict = {}
